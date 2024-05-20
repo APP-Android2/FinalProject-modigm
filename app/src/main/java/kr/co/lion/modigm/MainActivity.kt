8f81f54712743bb115217ddda69f66159e0b1522
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-05-20 15:00:39 +0900

    test

--- app/src/main/java/kr/co/lion/modigm/MainActivity.kt
+++ app/src/main/java/kr/co/lion/modigm/MainActivity.kt
@@ -16,5 +16,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 테스트 - 원빈
     }
 }
