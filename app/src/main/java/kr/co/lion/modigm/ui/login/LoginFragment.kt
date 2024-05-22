3bef05a1debf7cea5b1fb541d576b72605b7ad0d
tjddn <tjddnjspower@naver.com>
2024-05-22 15:34:50 +0900

    [feat] 소셜 로그인 화면 마진값 조정, 주석 작성

--- app/src/main/java/kr/co/lion/modigm/ui/login/LoginFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/login/LoginFragment.kt
@@ -1,13 +1,10 @@
 package kr.co.lion.modigm.ui.login
 
-import android.R
-import android.graphics.Bitmap
-import android.graphics.ColorFilter
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import kr.co.lion.modigm.databinding.FragmentLoginBinding
 
 
@@ -15,18 +12,22 @@ class LoginFragment : Fragment() {
 
     lateinit var binding : FragmentLoginBinding
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentLoginBinding.inflate(inflater,container,false)
         return binding.root
     }
 
+    // view
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // 초기 뷰 세팅
         initView()
     }
 
 
+    // 초기 뷰 세팅
     fun initView(){
 
         // 바인딩
