f37ee1eca72f8a15e075f650d67292cf12ebe7af
ums1212 <voll1212@gmail.com>
2024-05-23 09:45:09 +0900

    [Feat] #36 관심 분야 유효성 검사

--- app/src/main/java/kr/co/lion/modigm/ui/join/JoinStep3Fragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/join/JoinStep3Fragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.chip.Chip
+import kr.co.lion.modigm.R
 import kr.co.lion.modigm.databinding.FragmentJoinStep3Binding
 
 class JoinStep3Fragment : Fragment() {
@@ -18,12 +20,49 @@ class JoinStep3Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        settingChips()
         return binding.root
+    }
+
+    private fun settingChips(){
+        // 데이터는 추후 수정
+        val interestList = arrayListOf<String>(
+            "웹", "서버", "임베디드", "프론트 엔드", "백 엔드",
+            "iOS", "안드로이드", "C, C++", "파이썬", "하드웨어",
+            "머신러닝", "빅데이터", "Node.js", ".NET", "블록체인",
+            "크로스플랫폼", "그래픽스", "VR"
+        )
+        for(chipName in interestList){
+            binding.chipGroupJoinInterest.addView(
+                Chip(requireContext()).apply {
+                    text = chipName
+                    isCheckable = true
+                    setTextColor(resources.getColor(R.color.textGray, null))
+                    setChipBackgroundColorResource(R.color.white)
+                    setChipStrokeColorResource(R.color.buttonGray)
+                    setOnCheckedChangeListener { _, isChecked ->
+                        if(isChecked){
+                            setTextColor(resources.getColor(R.color.white, null))
+                            setChipBackgroundColorResource(R.color.pointColor)
+                        }else{
+                            setTextColor(resources.getColor(R.color.textGray, null))
+                            setChipBackgroundColorResource(R.color.white)
+                        }
+                    }
+                }
+            )
+        }
     }
 
     // 입력한 내용 유효성 검사
     fun validate(): Boolean {
-        return true
+        binding.textViewJoinAlert.visibility = View.GONE
+        return if(binding.chipGroupJoinInterest.checkedChipIds.isEmpty()){
+            binding.textViewJoinAlert.visibility = View.VISIBLE
+            false
+        }else{
+            true
+        }
     }
 
 }
