deec0f74fa8cd1f021597e4194f7cd136cc2b63e
ums1212 <voll1212@gmail.com>
2024-05-24 10:27:48 +0900

    [Fix] #49 중복 확인창에서 돌아올때 칩이 중복으로 생성되는 버그 수정

--- app/src/main/java/kr/co/lion/modigm/ui/join/JoinStep3Fragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/join/JoinStep3Fragment.kt
@@ -15,6 +15,14 @@ class JoinStep3Fragment : Fragment() {
         FragmentJoinStep3Binding.inflate(layoutInflater)
     }
 
+    // 데이터는 추후 수정
+    val interestList = arrayListOf(
+        "웹", "서버", "임베디드", "프론트 엔드", "백 엔드",
+        "iOS", "안드로이드", "C, C++", "파이썬", "하드웨어",
+        "머신러닝", "빅데이터", "Node.js", ".NET", "블록체인",
+        "크로스플랫폼", "그래픽스", "VR"
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,13 +33,7 @@ class JoinStep3Fragment : Fragment() {
     }
 
     private fun settingChips(){
-        // 데이터는 추후 수정
-        val interestList = arrayListOf(
-            "웹", "서버", "임베디드", "프론트 엔드", "백 엔드",
-            "iOS", "안드로이드", "C, C++", "파이썬", "하드웨어",
-            "머신러닝", "빅데이터", "Node.js", ".NET", "블록체인",
-            "크로스플랫폼", "그래픽스", "VR"
-        )
+        binding.chipGroupJoinInterest.removeAllViews()
         for(chipName in interestList){
             binding.chipGroupJoinInterest.addView(
                 Chip(requireContext()).apply {
