3c2dd07a0d020733a5a100c4d7bb26a451bbe7e6
ums1212 <voll1212@gmail.com>
2024-05-27 11:28:20 +0900

    [Feat] #79 Model 작업

--- app/src/main/java/kr/co/lion/modigm/model/UserInfoData.kt
+++ app/src/main/java/kr/co/lion/modigm/model/UserInfoData.kt
@@ -1,4 +1,13 @@
 package kr.co.lion.modigm.model
 
-class UserInfoData {
-}
+data class UserInfoData(
+    val userName: String,
+    val userPhone: String,
+    val userProfilePic: String? = null,
+    val userInterestList: List<String>,
+    val userIntro: String? = null,
+    val userLinkMap: Map<String, String>? = null,
+    val userNumber: String,
+    // 회원 가입 시 True로, 회원 가입 탈퇴 처리 시 False로 변경
+    val userState: Boolean,
+)
