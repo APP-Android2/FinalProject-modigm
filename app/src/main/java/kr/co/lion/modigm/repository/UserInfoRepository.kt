13bebd025453534f75c697c6f525745fa8cf0e49
ums1212 <voll1212@gmail.com>
2024-05-27 14:44:28 +0900

    [Feat] #79 회원가입 Repository

--- app/src/main/java/kr/co/lion/modigm/repository/UserInfoRepository.kt
+++ app/src/main/java/kr/co/lion/modigm/repository/UserInfoRepository.kt
@@ -1,6 +1,12 @@
 package kr.co.lion.modigm.repository
 
+import kr.co.lion.modigm.db.remote.JoinUserDataSource
+import kr.co.lion.modigm.model.UserInfoData
+
 class UserInfoRepository {
 
+    private val _joinUserDataSource = JoinUserDataSource()
 
+    // 회원가입
+    suspend fun insetUserData(userInfoData: UserInfoData): Boolean = _joinUserDataSource.insetUserData(userInfoData)
 }
