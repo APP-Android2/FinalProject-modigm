5a63496343a2c9fa7bde9453d3a664312295d036
ums1212 <voll1212@gmail.com>
2024-05-27 14:42:46 +0900

    [Feat] #79 관심 분야 enum class 추가 및 적용

--- app/src/main/java/kr/co/lion/modigm/ui/join/vm/JoinStep3ViewModel.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/join/vm/JoinStep3ViewModel.kt
@@ -3,27 +3,23 @@ package kr.co.lion.modigm.ui.join.vm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kr.co.lion.modigm.util.InterestList
 
 class JoinStep3ViewModel: ViewModel() {
 
     // 데이터는 추후 수정
     val interestList = MutableLiveData(
-        arrayListOf(
-            "웹", "서버", "임베디드", "프론트 엔드", "백 엔드",
-            "iOS", "안드로이드", "C, C++", "파이썬", "하드웨어",
-            "머신러닝", "빅데이터", "Node.js", ".NET", "블록체인",
-            "크로스플랫폼", "그래픽스", "VR"
-        )
+        InterestList.values()
     )
 
-    private val _selectedInterestList = MutableLiveData<ArrayList<String>>(arrayListOf())
-    val selectedInterestList: LiveData<ArrayList<String>> = _selectedInterestList
+    private val _selectedInterestList = MutableLiveData<MutableList<Int>>(mutableListOf())
+    val selectedInterestList: LiveData<MutableList<Int>> = _selectedInterestList
 
-    fun addInterest(interest: String) {
+    fun addInterest(interest: Int) {
         _selectedInterestList.value?.add(interest)
     }
 
-    fun removeInterest(interest: String) {
+    fun removeInterest(interest: Int) {
         _selectedInterestList.value?.remove(interest)
     }
 
