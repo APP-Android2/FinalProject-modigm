14b275b1e53cb4be2b2e6aca5d644c1b62c16c0f
ums1212 <voll1212@gmail.com>
2024-05-28 17:26:16 +0900

    [Fix] #79 회원 정보 Model 수정

--- app/src/main/java/kr/co/lion/modigm/model/UserInfoData.kt
+++ app/src/main/java/kr/co/lion/modigm/model/UserInfoData.kt
@@ -3,10 +3,10 @@ package kr.co.lion.modigm.model
 data class UserInfoData(
     val userName: String,
     val userPhone: String,
-    val userProfilePic: String?,
+    val userProfilePic: String,
     val userInterestList: MutableList<Int>,
-    val userIntro: String?,
-    val userLinkMap: MutableList<String>?,
+    val userIntro: String,
+    val userLinkMap: MutableList<String>,
     val userNumber: String,
     // 회원 가입 시 True로, 회원 가입 탈퇴 처리 시 False로 변경
     val userState: Boolean,
