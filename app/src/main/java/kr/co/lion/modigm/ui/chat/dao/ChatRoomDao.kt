f5835ddbdee1ca6e25f6d3b1101dd5a68c124c41
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-05-29 14:53:19 +0900

    [feat] DAO 코드 정리

--- app/src/main/java/kr/co/lion/modigm/ui/chat/dao/ChatRoomDao.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/chat/dao/ChatRoomDao.kt
@@ -1,9 +1,6 @@
 package kr.co.lion.modigm.ui.chat.dao
 
-import android.util.Log
 import com.google.firebase.Firebase
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.CoroutineScope
@@ -16,14 +13,18 @@ class ChatRoomDao {
 
     companion object{
 
-        // 채팅 방 시퀀스 번호를 Get 후 Int 형으로 반환
+        // 공통으로 쓰이는 collectionReferenceSequence - Sequence 로 설정
+        val collectionReferenceSequence = Firebase.firestore.collection("Sequence")
+        // 공통으로 쓰이는 collectionReference - ChatRoomData 로 설정
+        val collectionReference = Firebase.firestore.collection("ChatRoomData")
+
+        // 채팅 방 시퀀스 번호를 Get 후 반환함
         suspend fun getChatRoomSequence():Int{
 
             var chatRoomSequence = -1
 
             val coroutine1 = CoroutineScope(Dispatchers.IO).launch {
-                val collectionReference = Firebase.firestore.collection("Sequence")
-                val documentReference = collectionReference.document("ChatRoomSequence")
+                val documentReference = collectionReferenceSequence.document("ChatRoomSequence")
                 val documentSnapShot = documentReference.get().await()
                 chatRoomSequence = documentSnapShot.getLong("value")?.toInt()!!
             }
@@ -32,11 +33,10 @@ class ChatRoomDao {
             return chatRoomSequence
         }
 
-        // 채팅 방 시퀀스 값을 Update 한다.
+        // 채팅 방 시퀀스 값을 변경함 (Update)
         suspend fun updateChatRoomSequence(userSequence:Int){
             val coroutine1 = CoroutineScope(Dispatchers.IO).launch {
-                val collectionReference = Firebase.firestore.collection("Sequence")
-                val documentReference = collectionReference.document("ChatRoomSequence")
+                val documentReference = collectionReferenceSequence.document("ChatRoomSequence")
                 // 저장할 데이터를 담을 HashMap을 만들어준다.
                 val map = mutableMapOf<String, Long>()
                 map["value"] = userSequence.toLong()
@@ -46,22 +46,19 @@ class ChatRoomDao {
             coroutine1.join()
         }
 
-        // 채팅 방을 Create 한다.
+        // 채팅 방을 생성함 (Create)
         suspend fun insertChatRoomData(chatRoomData: ChatRoomData){
             val coroutine1 = CoroutineScope(Dispatchers.IO).launch {
-                val collectionReference = Firebase.firestore.collection("ChatRoomData")
                 collectionReference.add(chatRoomData)
             }
             coroutine1.join()
         }
 
-        // 단체 채팅방 데이터 가져오기
+        // 단체 채팅방 데이터 가져옴 (Read)
         suspend fun getGroupChatRooms(userId: String): MutableList<ChatRoomData> {
             var chatRooms = mutableListOf<ChatRoomData>()
 
             val coroutine1 = CoroutineScope(Dispatchers.IO).launch {
-                val collectionReference = Firebase.firestore.collection("ChatRoomData")
-
                 // 내 아이디의 chatIdx 와 groupChat이 true인 경우 필터링
                 // chatIdx는 UserData가 있다면 그 Data에서 아이디 별 소속해있는 채팅방 int형 list를 가져와야함.
                 val querySnapshot = collectionReference
@@ -83,13 +80,11 @@ class ChatRoomDao {
             return chatRooms
         }
 
-        // 1:1 채팅방 데이터 가져오기
+        // 1:1 채팅방 데이터 가져옴 (Read)
         suspend fun getOneToOneChatRooms(userId: String): MutableList<ChatRoomData> {
             var chatRooms = mutableListOf<ChatRoomData>()
 
             val coroutine1 = CoroutineScope(Dispatchers.IO).launch {
-                val collectionReference = Firebase.firestore.collection("ChatRoomData")
-
                 // 내 아이디의 chatIdx 와 groupChat이 true인 경우 필터링
                 // chatIdx는 UserData가 있다면 그 Data에서 아이디 별 소속해있는 채팅방 int형 list를 가져와야함.
                 val querySnapshot = collectionReference
@@ -111,13 +106,11 @@ class ChatRoomDao {
             return chatRooms
         }
 
-        // 해당 채팅방 마지막 메세지, 마지막 메세지 시간 저장
+        // 해당 채팅방 데이터 [마지막 메세지, 마지막 메세지 시간] 변경함 (Update)
         suspend fun updateChatRoomLastMessageAndTime(chatIdx: Int, chatMessage: String, chatFullTime: Long, chatTime: String): MutableList<ChatRoomData> {
             var chatRooms = mutableListOf<ChatRoomData>()
 
             val coroutine1 = CoroutineScope(Dispatchers.IO).launch {
-                val collectionReference = Firebase.firestore.collection("ChatRoomData")
-
                 // 내 아이디의 chatIdx 와 groupChat이 true인 경우 필터링
                 // chatIdx는 UserData가 있다면 그 Data에서 아이디 별 소속해있는 채팅방 int형 list를 가져와야함.
                 val querySnapshot = collectionReference
@@ -141,11 +134,9 @@ class ChatRoomDao {
             return chatRooms
         }
 
-        // chatMemberList 배열에서 내 ID를 제거
+        // 채팅방 나가기 / chatMemberList 배열에서 내 ID를 제거 (Update)
         suspend fun removeUserFromChatMemberList(chatIdx: Int, userId: String) {
             val coroutine1 = CoroutineScope(Dispatchers.IO).launch {
-                val collectionReference = Firebase.firestore.collection("ChatRoomData")
-
                 val querySnapshot = collectionReference
                     .whereEqualTo("chatIdx", chatIdx)
                     .get()
@@ -161,11 +152,10 @@ class ChatRoomDao {
             coroutine1.join()
         }
 
-        // 사용자가 메세지 읽음 처리
+        // 로그인 한 사용자 해당 채팅 방 메세지 읽음 처리 (Update)
         suspend fun chatRoomMessageAsRead(chatIdx: Int, loginUserId: String) {
             val coroutine1 = CoroutineScope(Dispatchers.IO).launch {
-                val chatRoomRef = Firebase.firestore.collection("ChatRoomData")
-                    .whereEqualTo("chatIdx", chatIdx)
+                val chatRoomRef = collectionReference.whereEqualTo("chatIdx", chatIdx)
                 val querySnapshot = chatRoomRef.get().await()
                 querySnapshot.forEach { document ->
                     val chatRoom = document.toObject(ChatRoomData::class.java)
@@ -178,16 +168,14 @@ class ChatRoomDao {
             coroutine1.join()
         }
 
-        // 새로운 메시지 도착 시 안 읽은 메시지 개수 증가
+        // 메세지 전송시 해당 채팅 방 사용자에 안읽은 메세지 카운트 증가 
         suspend fun increaseUnreadMessageCount(chatIdx: Int, senderId: String) {
             val coroutine1 = CoroutineScope(Dispatchers.IO).launch {
-                val chatRoomRef = Firebase.firestore.collection("ChatRoomData")
-                    .whereEqualTo("chatIdx", chatIdx)
+                val chatRoomRef = collectionReference.whereEqualTo("chatIdx", chatIdx)
                 val querySnapshot = chatRoomRef.get().await()
                 querySnapshot.forEach { document ->
                     val chatRoom = document.toObject(ChatRoomData::class.java)
                     chatRoom?.let {
-                        val now = System.currentTimeMillis()
                         for (participant in it.chatMemberList) {
                             // 현재 입장 여부 확인
 //                            if (participant != senderId && it.chatMemberState[participant] == false) {
@@ -207,7 +195,6 @@ class ChatRoomDao {
         // 멤버 입장 여부 업데이트
         suspend fun updateMemberState(chatIdx: Int, memberId: String, isPresent: Boolean) {
             val coroutine = CoroutineScope(Dispatchers.IO).launch {
-                val collectionReference = Firebase.firestore.collection("ChatRoomData")
                 val chatRoomRef = collectionReference.whereEqualTo("chatIdx", chatIdx)
                 val querySnapshot = chatRoomRef.get().await()
                 querySnapshot.forEach { document ->
