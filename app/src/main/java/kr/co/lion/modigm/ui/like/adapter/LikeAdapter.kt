5aaf872c952d8f0e23c83c0b021db66382db0cb6
Lee SeungHyun <leesh0545@gmail.com>
2024-05-31 16:43:39 +0900

    [Feat #113] LikeAdapter 구현 및 각 데이터설정

---
+++ app/src/main/java/kr/co/lion/modigm/ui/like/adapter/LikeAdapter.kt
@@ -0,0 +1,95 @@
+package kr.co.lion.modigm.ui.like.adapter
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.lion.modigm.R
+import kr.co.lion.modigm.databinding.RowLikeBinding
+import kr.co.lion.modigm.model.StudyData
+
+class LikeAdapter(private val studyList: List<StudyData>) : RecyclerView.Adapter<LikeAdapter.StudyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudyViewHolder {
+        val binding = RowLikeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StudyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: StudyViewHolder, position: Int) {
+        holder.bind(studyList[position])
+    }
+
+    override fun getItemCount(): Int = studyList.size
+
+
+
+    inner class StudyViewHolder(private val binding: RowLikeBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(study: StudyData) {
+            binding.apply {
+
+                imageViewLikeCover.setImageResource(R.drawable.image_detail_1)
+
+                // 모집 상태에 따라 텍스트와 색상 설정
+                textViewLikeState.text = if (study.studyState) "모집 중" else "모집 마감"
+                val textColor = if (study.studyState) ContextCompat.getColor(itemView.context, R.color.pointColor)
+                else ContextCompat.getColor(itemView.context, R.color.dividerView)
+                textViewLikeState.setTextColor(textColor)
+
+                // 스터디 진행 방식
+                textViewLikeStateMeet.text = when (study.studyOnOffline) {
+                    0 -> "오프라인"
+                    1 -> "온라인"
+                    else -> "온오프혼합"
+                }
+
+                // 모집 상태에 따라 진행 방식 텍스트 색상 설정
+                textViewLikeStateMeet.setTextColor(
+                    if (study.studyState) {
+                        when (study.studyOnOffline) {
+                            0 -> Color.parseColor("#EB9C58") // 오프라인 색상
+                            1 -> Color.parseColor("#0FA981") // 온라인 색상
+                            else -> Color.parseColor("#0096FF") // 온오프혼합 색상
+                        }
+                    } else {
+                        ContextCompat.getColor(itemView.context, R.color.dividerView) // 모집 종료 시 회색
+                    }
+                )
+
+                // 스터디 제목
+                textViewLikeTitle.text = study.studyTitle
+
+                // 스터디 종류
+                textViewLikeStatePeriod.apply {
+                    text = when (study.studyType) {
+                        0 -> "스터디"
+                        1 -> "프로젝트"
+                        else -> "공모전"
+                    }
+                }
+                // 스터디 신청 인원
+                textViewLikeJoinMember.text = study.studyUidList.size.toString()
+
+                // 스터디 최대 인원
+                textViewLikeTotalMember.text = study.studyMaxMember.toString()
+
+                // 스터디 종류에 따라 아이콘 변경
+                imageViewLikeStatePeriod.setImageResource(
+                    when (study.studyType) {
+                        0 -> R.drawable.icon_closed_book_24px
+                        1 -> R.drawable.icon_code_box_24px
+                        else -> R.drawable.icon_trophy_24px
+                    }
+                )
+
+                // 스터디 신청 방식에 따른 텍스트 설정
+                textViewLikeApplyType.text = when (study.studyApplyMethod) {
+                    0 -> "신청제"
+                    1 -> "선착순"
+                    else -> "기타"
+                }
+            }
+        }
+    }
+
+}
