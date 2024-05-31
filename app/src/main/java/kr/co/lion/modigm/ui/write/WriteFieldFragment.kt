1f4306a62e8cc1f03c975cc9c9ae5e02592adec2
TedMoon99 <mtj1015@naver.com>
2024-05-31 14:40:54 +0900

    [Feat] WriteFieldFragment에 WriteViewModel관련 코드 추가

--- app/src/main/java/kr/co/lion/modigm/ui/write/WriteFieldFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/write/WriteFieldFragment.kt
@@ -1,28 +1,24 @@
 package kr.co.lion.modigm.ui.write
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import kr.co.lion.modigm.R
-import kr.co.lion.modigm.databinding.FragmentWriteBinding
 import kr.co.lion.modigm.databinding.FragmentWriteFieldBinding
-import kr.co.lion.modigm.ui.MainActivity
+import kr.co.lion.modigm.ui.write.vm.WriteViewModel
 
 class WriteFieldFragment : Fragment() {
 
-    private lateinit var fragmentWriteBinding: FragmentWriteBinding
     lateinit var fragmentWriteFieldBinding: FragmentWriteFieldBinding
-    private lateinit var mainActivity: MainActivity
+    private val viewModel: WriteViewModel by activityViewModels()
+
+    val fragmentName = "field"
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        // Progress Bar 변경을 위한 바인딩 불러오기
-        fragmentWriteBinding = FragmentWriteBinding.inflate(inflater)
-
-        mainActivity = activity as MainActivity
         fragmentWriteFieldBinding = FragmentWriteFieldBinding.inflate(inflater)
 
         settingView()
@@ -34,8 +30,7 @@ class WriteFieldFragment : Fragment() {
     fun settingView(){
         fragmentWriteFieldBinding.apply {
 
-            // progress 게이지 설정
-            fragmentWriteBinding.progressBarWriteFragment.progress = 20
+
         }
     }
     fun settingEvent(){
@@ -46,16 +41,22 @@ class WriteFieldFragment : Fragment() {
 
     // 카드뷰 클릭시 효과 설정
     fun cardViewEffect(){
-        val clickedStrokeColor = ContextCompat.getColor(mainActivity, R.color.pointColor)
-        val unclickedStrokeColor = ContextCompat.getColor(mainActivity, R.color.textGray)
+        val context = requireContext()
+        val clickedStrokeColor = ContextCompat.getColor(context, R.color.pointColor)
+        val unclickedStrokeColor = ContextCompat.getColor(context, R.color.textGray)
 
         // 스터디 선택 시 클릭 리스너
         fragmentWriteFieldBinding.apply {
             cardviewWriteFieldStudy.setOnClickListener {
                 cardviewWriteFieldStudy.apply {
                     if (cardElevation == 20F && strokeColor == clickedStrokeColor){
+                        // 애니메이션 효과 제거
                         cardElevation = 0F
                         strokeColor = unclickedStrokeColor
+
+                        // 사용자 입력 해제
+                        viewModel.userDidNotAnswer(fragmentName)
+                        viewModel.deactivateButton() // buttonState -> false
                     } else {
 
                         // Stroke 색상 변경
@@ -67,6 +68,10 @@ class WriteFieldFragment : Fragment() {
                         cardElevation = 20F
                         cardviewWriteFieldContest.cardElevation = 0F
                         cardviewWriteFieldProject.cardElevation = 0F
+
+                        // 사용자 입력 추가
+                        viewModel.userDidAnswer(fragmentName)
+                        viewModel.activateButton() // buttonState -> false
                     }
                 }
             }
@@ -75,8 +80,13 @@ class WriteFieldFragment : Fragment() {
             cardviewWriteFieldContest.setOnClickListener {
                 cardviewWriteFieldContest.apply {
                     if (cardElevation == 20F && strokeColor == clickedStrokeColor){
+                        // 애니메이션 효과 제거
                         cardElevation = 0F
                         strokeColor = unclickedStrokeColor
+
+                        // 사용자 입력 해제
+                        viewModel.userDidNotAnswer(fragmentName)
+                        viewModel.deactivateButton()
                     } else {
                         // Stroke 색상 변경
                         strokeColor = clickedStrokeColor
@@ -87,6 +97,11 @@ class WriteFieldFragment : Fragment() {
                         cardElevation = 20F
                         cardviewWriteFieldStudy.cardElevation = 0F
                         cardviewWriteFieldProject.cardElevation = 0F
+
+                        // 사용자 입력 추가
+                        viewModel.userDidAnswer(fragmentName)
+                        viewModel.activateButton()
+                        viewModel.deactivateButton()
                     }
                 }
             }
@@ -95,8 +110,12 @@ class WriteFieldFragment : Fragment() {
             cardviewWriteFieldProject.setOnClickListener {
                 cardviewWriteFieldProject.apply {
                     if (cardElevation == 20F && strokeColor == clickedStrokeColor){
+                        // 애니메이션 효과 제거
                         cardElevation = 0F
                         strokeColor = unclickedStrokeColor
+
+                        // 사용자 입력 해제
+                        viewModel.userDidNotAnswer(fragmentName)
                     } else {
 
                         // Stroke 색상 변경
@@ -108,25 +127,14 @@ class WriteFieldFragment : Fragment() {
                         cardElevation = 20F
                         cardviewWriteFieldContest.cardElevation = 0F
                         cardviewWriteFieldStudy.cardElevation = 0F
+
+                        // 사용자 입력 추가
+                        viewModel.userDidAnswer(fragmentName)
+                        viewModel.activateButton()
                     }
                 }
             }
 
-
-            buttonWriteFieldNext.setOnClickListener {
-                // 다음 탭으로 이동
-                fragmentWriteBinding.apply {
-                    Log.d("TedMoon", "다음 탭으로 이동1")
-                    val currentItem = viewPagerWriteFragment.currentItem
-                    Log.d("TedMoon", "다음 탭으로 이동2")
-                    if (currentItem < viewPagerWriteFragment.adapter!!.itemCount - 1){
-                        Log.d("TedMoon", "다음 탭으로 이동2-1")
-                        viewPagerWriteFragment.currentItem = currentItem + 1
-                        Log.d("TedMoon", "다음 탭으로 이동2-2")
-                    }
-                    Log.d("TedMoon", "다음 탭으로 이동 완료")
-                }
-            }
         }
     }
 }
