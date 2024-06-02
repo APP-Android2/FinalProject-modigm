9e54059cd94b8a022145cdfef4d8d367765b25d7
TedMoon99 <mtj1015@naver.com>
2024-06-02 23:49:01 +0900

    [Fix] 스킬 탭 - MainActivty 코드 제거

--- app/src/main/java/kr/co/lion/modigm/ui/write/WriteIntroFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/write/WriteIntroFragment.kt
@@ -1,10 +1,14 @@
 package kr.co.lion.modigm.ui.write
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import kr.co.lion.modigm.Manifest
 import kr.co.lion.modigm.databinding.FragmentWriteIntroBinding
 import kr.co.lion.modigm.ui.MainActivity
 import kr.co.lion.modigm.ui.write.more.CustomDialogWriteIntroExample
@@ -12,13 +16,11 @@ import kr.co.lion.modigm.ui.write.more.CustomDialogWriteIntroExample
 class WriteIntroFragment : Fragment() {
 
     lateinit var fragmentWriteIntroBinding: FragmentWriteIntroBinding
-    lateinit var mainActivity: MainActivity
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        mainActivity = activity as MainActivity
         fragmentWriteIntroBinding = FragmentWriteIntroBinding.inflate(inflater)
         return fragmentWriteIntroBinding.root
     }
@@ -33,17 +35,23 @@ class WriteIntroFragment : Fragment() {
     fun settingEvent(){
         fragmentWriteIntroBinding.apply {
             // image 버튼 클릭 리스너
-            imageButtonWriteIntroCoverImage.setOnClickListener {
-                // 파일을 불러들인다
+            imageViewWriteIntroCoverImage.setOnClickListener {
+
             }
 
             // 작성예시 클릭 리스너
             textViewWriteIntroWriteExample.setOnClickListener {
                 // 다이얼로그를 띄워준다
-                val dialog = CustomDialogWriteIntroExample(mainActivity)
+                val context = requireContext()
+                val dialog = CustomDialogWriteIntroExample(context)
                 dialog.show()
             }
         }
+
+    }
+
+    // 사진 등록
+    fun settingPicture(){
 
     }
 
