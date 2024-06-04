106a18483745dd90f214088789be002715e5dc9a
language7606 <tjddnjspower@naver.com>
2024-06-04 21:03:22 +0900

    [Feat] 깃허브 로그인 디버깅 및 MVVM 적용

--- app/src/main/java/kr/co/lion/modigm/repository/UserInfoRepository.kt
+++ app/src/main/java/kr/co/lion/modigm/repository/UserInfoRepository.kt
@@ -1,5 +1,6 @@
 package kr.co.lion.modigm.repository
 
+import android.app.Activity
 import kr.co.lion.modigm.db.user.RemoteUserDataSource
 import kr.co.lion.modigm.model.UserData
 
@@ -9,4 +10,18 @@ class UserInfoRepository {
 
     // 회원가입
     suspend fun insetUserData(userInfoData: UserData): Boolean = _joinUserDataSource.insetUserData(userInfoData)
+
+
+    // ----------------- 로그인 데이터 처리 -----------------
+
+    // Firebase Functions를 통해 Custom Token 획득
+    suspend fun getKakaoCustomToken(accessToken: String): String = _joinUserDataSource.getKakaoCustomToken(accessToken)
+
+    // Firebase Custom Token으로 로그인
+    suspend fun signInWithCustomToken(customToken: String) = _joinUserDataSource.signInWithCustomToken(customToken)
+
+    // 깃허브 로그인
+    suspend fun signInWithGithub(context: Activity) = _joinUserDataSource.signInWithGithub(context)
+
+    // ----------------- 로그인 데이터 처리 끝-----------------
 }
