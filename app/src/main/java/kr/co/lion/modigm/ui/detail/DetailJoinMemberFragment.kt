053dc80a201ebde345d90271d8917ff526fa7062
Lee SeungHyun <leesh0545@gmail.com>
2024-06-09 21:54:25 +0900

    [feat #170] studyIdx 값 넘겨주기

--- app/src/main/java/kr/co/lion/modigm/ui/detail/DetailJoinMemberFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/detail/DetailJoinMemberFragment.kt
@@ -11,24 +11,20 @@ import kr.co.lion.modigm.databinding.FragmentDetailJoinMemberBinding
 import kr.co.lion.modigm.ui.MainActivity
 import kr.co.lion.modigm.ui.detail.adapter.DetailJoinMembersAdapter
 
-data class Member(
-    val name: String,
-    val intro: String
-)
-
 class DetailJoinMemberFragment : Fragment() {
 
     lateinit var fragmentDetailJoinMemberBinding: FragmentDetailJoinMemberBinding
 
-    lateinit var mainActivity: MainActivity
+    // 현재 선택된 스터디 idx 번호를 담을 변수(임시)
+    var studyIdx = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         fragmentDetailJoinMemberBinding = FragmentDetailJoinMemberBinding.inflate(layoutInflater)
-        mainActivity = activity as MainActivity
-
+        // 상품 idx
+        studyIdx = arguments?.getInt("studyIdx")!!
 
         return fragmentDetailJoinMemberBinding.root
     }
@@ -43,14 +39,7 @@ class DetailJoinMemberFragment : Fragment() {
     fun setupRecyclerView() {
         val layoutManager = LinearLayoutManager(context)
         fragmentDetailJoinMemberBinding.recyclerviewDetailJoin.layoutManager = layoutManager
-        // 임시 데이터(확인용)
-        val members = listOf(
-            Member("홍길동", "열심히 일하는 개발자입니다."),
-            Member("김철수", "디자인을 사랑하는 크리에이터입니다."),
-            Member("이영희", "프로젝트 매니지먼트가 전문인 매니저입니다.")
-        )
-        fragmentDetailJoinMemberBinding.recyclerviewDetailJoin.adapter = DetailJoinMembersAdapter(members)
-        Log.d("DetailJoinMemberFragment", "Adapter set with ${members.size} members.")
+
     }
 
 
