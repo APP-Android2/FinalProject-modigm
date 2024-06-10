85213d03d2b3995bba2cd8e53d95b728382d3d5e
ums1212 <voll1212@gmail.com>
2024-06-10 10:39:36 +0900

    [Chore] #176 작업 전

--- app/src/main/java/kr/co/lion/modigm/ui/profile/ChangePwFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/profile/ChangePwFragment.kt
@@ -1,40 +1,84 @@
 package kr.co.lion.modigm.ui.profile
 
+import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 import kr.co.lion.modigm.R
 import kr.co.lion.modigm.databinding.FragmentChangePwBinding
 import kr.co.lion.modigm.ui.profile.vm.ChangePwViewModel
 
 class ChangePwFragment : Fragment() {
 
-    lateinit var binding: FragmentChangePwBinding
-    private val viewModel: ChangePwViewModel by viewModels()
+    // lateinit var binding: FragmentChangePwBinding
+    // private val viewModel: ChangePwViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_change_pw, container, false)
-        binding.changePwViewModel = viewModel
-        binding.lifecycleOwner = this
+//        // Inflate the layout for this fragment
+//        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_change_pw, container, false)
+//        binding.changePwViewModel = viewModel
+//        binding.lifecycleOwner = this
+//
+//        settingErrorText()
+//        settingChangePWButtonDone()
 
-        settingErrorText()
-
-        return binding.root
+//         return binding.root
+        return null
     }
 
     private fun settingErrorText(){
-        binding.changePWTextFieldPW.error = viewModel.oldPwError.value
-        binding.changePWTextFieldNewPW.error = viewModel.newPwError.value
-        binding.changePWTextFieldCheck.error = viewModel.newPwCheckError.value
+//        binding.changePWTextFieldPW.error = viewModel.oldPwError.value
+//        binding.changePWTextFieldNewPW.error = viewModel.newPwError.value
+//        binding.changePWTextFieldCheck.error = viewModel.newPwCheckError.value
     }
 
+    private fun settingChangePWButtonDone(){
+//        binding.changePWButtonDone.setOnClickListener {
+//            lifecycleScope.launch {
+//                val result = viewModel.changePw()
+//                if(result){
+//                    parentFragmentManager.popBackStack()
+//                }else{
+//                    showSnackBar()
+//                }
+//            }
+//        }
+    }
+
+    private fun showSnackBar() {
+//
+//        val snackbar =
+//            Snackbar.make(binding.root, "비밀번호 변경 실패", Snackbar.LENGTH_SHORT)
+//
+//        // 스낵바의 뷰를 가져옵니다.
+//        val snackbarView = snackbar.view
+//
+//        // 스낵바 텍스트 뷰 찾기
+//        val textView =
+//            snackbarView.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+//
+//        // 텍스트 크기를 dp 단위로 설정
+//        val textSizeInPx = dpToPx(requireContext(), 16f)
+//        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSizeInPx)
+//
+//        snackbar.show()
+    }
+
+    // 스낵바 글시 크기 설정을 위해 dp를 px로 변환
+    private fun dpToPx(context: Context, dp: Float): Float {
+        return dp * context.resources.displayMetrics.density
+    }
 
 }
--- app/src/main/java/kr/co/lion/modigm/ui/profile/vm/ChangePwViewModel.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/profile/vm/ChangePwViewModel.kt
@@ -1,8 +1,12 @@
 package kr.co.lion.modigm.ui.profile.vm
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class ChangePwViewModel: ViewModel() {
     // 파이어베이스 인증
@@ -25,4 +29,64 @@ class ChangePwViewModel: ViewModel() {
     // 현재 비밀번호 에러 메시지
     val newPwCheckError = MutableLiveData<String>()
 
+
+    // 비밀번호 유효성 검사
+    private fun checkValidation(): Boolean{
+        oldPwError.value = ""
+        newPwError.value = ""
+        newPwCheckError.value = ""
+
+        var result = false
+
+        if(oldPw.value.isNullOrEmpty()) {
+            oldPwError.value = "현재 비밀번호를 입력해주세요."
+            result = true
+        }
+        if(newPw.value.isNullOrEmpty()) {
+            newPwError.value = "새로운 비밀번호를 입력해주세요."
+            result = true
+        }
+        if(newPwCheck.value.isNullOrEmpty()) {
+            newPwCheckError.value = "새로운 비밀번호 확인을 입력해주세요."
+            result = true
+        }
+        if(newPw.value != newPwCheck.value) {
+            newPwCheckError.value = "새로운 비밀번호가 일치하지 않습니다."
+            result = true
+        }
+
+        return result
+    }
+
+    // 비밀번호 변경
+    suspend fun changePw(): Boolean {
+        var result = false
+
+        // 유효성 검사
+        if(checkValidation()) return false
+
+        viewModelScope.launch {
+            // 다시 로그인을 시도해서 현재 비밀번호가 일치하는지 확인
+            try{
+                _auth.signInWithEmailAndPassword(_user?.email?:"", oldPw.value?:"").await()
+            }catch (e: Exception){
+                oldPwError.value = "현재 비밀번호가 일치하지 않습니다."
+                Log.d("changePw","비밀번호 확인 실패 : ${_auth.currentUser?.email}")
+                result = false
+                return@launch
+            }
+
+            // 새로운 비밀번호로 업데이트
+            try {
+                _user?.updatePassword(newPw.value?:"")?.await()
+                result = true
+            }catch (e: Exception){
+                Log.d("changePw", e.toString())
+                result = false
+                return@launch
+            }
+        }
+
+        return result
+    }
 }
