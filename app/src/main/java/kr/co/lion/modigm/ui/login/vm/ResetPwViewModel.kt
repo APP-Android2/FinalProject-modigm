baabcb3e0b90fe56866ee4e757f7386f76ff817c
ums1212 <voll1212@gmail.com>
2024-06-12 09:12:15 +0900

    [Fix] #232 유효성 검사 수정

--- app/src/main/java/kr/co/lion/modigm/ui/login/vm/ResetPwViewModel.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/login/vm/ResetPwViewModel.kt
@@ -3,14 +3,14 @@ package kr.co.lion.modigm.ui.login.vm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
+import java.util.regex.Pattern
 
 class ResetPwViewModel: ViewModel() {
 
     private val _auth = FirebaseAuth.getInstance()
+
+    private val _oldPassword = MutableLiveData<String>()
 
     val newPassword = MutableLiveData<String>()
     val newPasswordError = MutableLiveData<String>()
@@ -26,6 +26,11 @@ class ResetPwViewModel: ViewModel() {
         if(newPassword.value.isNullOrEmpty()){
             newPasswordError.value = "비밀번호를 입력해주세요."
             return false
+        }else{
+            if(!Pattern.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{8,20}$", newPassword.value)){
+                newPasswordError.value = "영문, 숫자, 특수문자가 포함된 비밀번호를 8~20자로 입력해주세요."
+                return false
+            }
         }
         if(newPasswordCheck.value.isNullOrEmpty()){
             newPasswordCheckError.value = "비밀번호를 확인해주세요."
