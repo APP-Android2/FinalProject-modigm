a349f1ca8053f61b426ee4af15b895a921c79d91
Lee SeungHyun <leesh0545@gmail.com>
2024-06-12 22:14:36 +0900

    [fix #238] WritePeriodFragment 수정

--- app/src/main/java/kr/co/lion/modigm/ui/write/WritePeriodFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/write/WritePeriodFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -12,126 +13,79 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kr.co.lion.modigm.R
 import kr.co.lion.modigm.databinding.DialogWritePeriodFragmentBinding
 import kr.co.lion.modigm.databinding.FragmentWritePeriodBinding
+import kr.co.lion.modigm.ui.write.adapter.OptionSpinnerAdapter
 import kr.co.lion.modigm.ui.write.vm.WriteViewModel
 
 
 class WritePeriodFragment : Fragment() {
 
-    lateinit var fragmentWritePeriodBinding: FragmentWritePeriodBinding
+    lateinit var binding: FragmentWritePeriodBinding
     private val viewModel: WriteViewModel by activityViewModels()
     val tabName = "period"
+
+    var selectPeriod :String = ""
+
+    // 선택된 기간에 해당하는 숫자를 저장하는 배열
+    val periodTags = listOf(0, 1, 2, 3, 4, 5, 6, 7)
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        fragmentWritePeriodBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_write_period, container, false)
-        fragmentWritePeriodBinding.writeViewModel = viewModel
-        fragmentWritePeriodBinding.lifecycleOwner = this
+        binding = FragmentWritePeriodBinding.inflate(inflater)
 
-        return fragmentWritePeriodBinding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initData()
-        settingEvent()
-        getAnswer()
+        settingWritePeriodSpinner()
     }
 
-    fun initData() {
-        // 입력 초기화
-        viewModel.userDidNotAnswer(tabName)
+    fun settingWritePeriodSpinner() {
+        val periodRequests = listOf(
+            "기간 선택",
+            "1개월 이하",
+            "2개월 이하",
+            "3개월 이하",
+            "4개월 이하",
+            "5개월 이하",
+            "6개월 미만",
+            "6개월 이상"
+        )
+        val spinnerAdapter = OptionSpinnerAdapter(
+            requireContext(),
+            R.layout.spinner,
+            periodRequests
+        )
+        binding.SpinnerWritePeriod.adapter = spinnerAdapter
 
-        // 전에 받은 입력이 있다면~
-        if (viewModel.periodClicked.value == true) {
-            // 버튼을 활성화
-            viewModel.activateButton()
-        } else {
-            // 버튼을 비활성화
-            viewModel.deactivateButton()
-        }
 
-    }
+        binding.SpinnerWritePeriod.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
 
-
-    fun settingEvent() {
-        // 기간선택 클릭시
-        fragmentWritePeriodBinding.apply {
-            // 다이얼로그를 띄운다
-            textinputWritePeriod.setOnClickListener {
-                val context = requireContext() // 이 Fragment의 context가져오기
-                val builder = MaterialAlertDialogBuilder(context)
-
-                // 뷰를 설정한다
-                val dialogWritePeriodFragmentBinding =
-                    DialogWritePeriodFragmentBinding.inflate(layoutInflater)
-                builder.setView(dialogWritePeriodFragmentBinding.root)
-                val dialog = builder.create()
-
-                // 버튼 클릭 이벤트 처리
-                dialogWritePeriodFragmentBinding.apply {
-                    textViewDialogWritePeriod01.setOnClickListener {
-
-                        clickAnimation(it)
-                        textinputWritePeriod.setText("1개월 이하")
-                        viewModel.gettingStudyPeriod(1)
-                        dialog.dismiss()
-                    }
-
-                    textViewDialogWritePeriod02.setOnClickListener {
-                        clickAnimation(it)
-                        textinputWritePeriod.setText("1개월 이상")
-                        viewModel.gettingStudyPeriod(2)
-                        dialog.dismiss()
-                    }
-
-                    textViewDialogWritePeriod03.setOnClickListener {
-                        clickAnimation(it)
-                        textinputWritePeriod.setText("3개월 이상")
-                        viewModel.gettingStudyPeriod(3)
-                        dialog.dismiss()
-                    }
-
-                    textViewDialogWritePeriod04.setOnClickListener {
-                        clickAnimation(it)
-                        textinputWritePeriod.setText("6개월 이상")
-                        viewModel.gettingStudyPeriod(4)
-                        dialog.dismiss()
+                    // 선택된 위치의 태그를 가져오기
+                    val tag = periodTags[position]
+                    selectPeriod = periodRequests[position]
+                    if (position == 0) {
+                        viewModel.validatePeriod(false)
+                    } else {
+                        viewModel.selectedPeriodTag.value = tag
+                        viewModel.validatePeriod(true)
                     }
                 }
 
-                dialog.show()
-            }
-        }
-    }
-
-    //
-    private fun getAnswer() {
-        // writePeriod Observing!
-        viewModel.studyPeriod.observe(viewLifecycleOwner) { period ->
-            when (period) {
-                0 -> {
-                    val toast = Toast.makeText(context, "활동하실 기간을 선택해주세요", Toast.LENGTH_SHORT)
-                    toast.show()
-                    // 미입력
-                    viewModel.userDidNotAnswer(tabName)
-                }
-
-                1, 2, 3, 4 -> {
-                    // 입력
-                    viewModel.userDidAnswer(tabName)
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // 아무것도 선택되지 않은 경우 처리
+                    viewModel.validatePeriod(false)
                 }
             }
-        }
-    }
-
-    // textView 클릭 시 애니메이션 처리
-    private fun clickAnimation(view: View) {
-        view.animate().scaleX(0.9f).scaleY(1.1f).setDuration(150).withEndAction {
-            view.animate().scaleX(1f).scaleY(1f).setDuration(150)
-        }
-
     }
 }
