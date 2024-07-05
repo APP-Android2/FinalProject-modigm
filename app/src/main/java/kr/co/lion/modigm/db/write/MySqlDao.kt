1286fc8780f205b19c0292b5ec3007a45fcbda21
ums1212 <voll1212@gmail.com>
2024-07-06 00:02:46 +0900

    [Fix] #240 mysqldao 수정

--- app/src/main/java/kr/co/lion/modigm/db/write/MySqlDao.kt
+++ app/src/main/java/kr/co/lion/modigm/db/write/MySqlDao.kt
@@ -1,6 +1,10 @@
-package kr.co.lion.modigm.db
+package kr.co.lion.modigm.db.write
 
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kr.co.lion.modigm.BuildConfig
 import kr.co.lion.modigm.model.StudyData
 import java.sql.Connection
@@ -23,11 +27,11 @@ class MySqlConn {
         connection = null
     }
 
-    fun insertStudyData(model: StudyData){
+    suspend fun insertStudyData(model: StudyData):Int?{
         var preparedStatement: PreparedStatement? = null
         val columns = model.getColumns()
         val values = model.getValues()
-
+        var idx:Int? = null
         try {
             val columnsString = StringBuilder()
             val valuesString = StringBuilder()
@@ -40,21 +44,31 @@ class MySqlConn {
             valuesString.deleteCharAt(valuesString.length-1)
             val sql = "INSERT INTO Study ($columnsString) VALUES ($valuesString)"
 
-            getConnection()
-            preparedStatement = connection?.prepareStatement(sql) // PreparedStatement 생성
-            values.forEachIndexed { index, value ->
-                // 쿼리 매개변수 설정
-                if(value is String){
-                    preparedStatement?.setString(index+1, value)
+            val deferred = CoroutineScope(Dispatchers.IO).async {
+                getConnection()
+                preparedStatement = connection?.prepareStatement(sql) // PreparedStatement 생성
+                values.forEachIndexed { index, value ->
+                    // 쿼리 매개변수 설정
+                    if(value is String){
+                        preparedStatement?.setString(index+1, value)
+                    }
+                    if(value is Int){
+                        preparedStatement?.setInt(index+1, value)
+                    }
+                    if(value is Boolean){
+                        preparedStatement?.setBoolean(index+1, value)
+                    }
                 }
-                if(value is Int){
-                    preparedStatement?.setInt(index+1, value)
-                }
-                if(value is Boolean){
-                    preparedStatement?.setBoolean(index+1, value)
-                }
+                preparedStatement?.executeUpdate() // 쿼리 실행
+
+                // insert된 행의 idx를 가져온다.
+                // == 마지막으로 insert된 행의 auto_increment 값을 가져온다.
+                val afterExecute = connection?.prepareStatement("SELECT LAST_INSERT_ID()")
+                val resultSet = afterExecute?.executeQuery()
+                resultSet?.next()
+                if(resultSet != null) idx = resultSet.getInt("LAST_INSERT_ID()")
             }
-            preparedStatement?.executeUpdate() // 쿼리 실행
+            awaitAll(deferred)
         } catch (e: Exception) {
             Log.e(TAG, "Error in insertStudyData", e) // 오류 로그 출력
         } finally {
@@ -65,6 +79,7 @@ class MySqlConn {
                 Log.e(TAG, "Error closing resources", e) // 리소스 닫기 오류 로그 출력
             }
         }
+        return idx
     }
 
     fun selectStudyOneByIdx(idx:Int): StudyData?{
