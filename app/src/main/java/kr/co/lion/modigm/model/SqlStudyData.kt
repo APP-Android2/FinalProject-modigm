e59d57b0ecae2e78523f89aa2157373e58df90a0
ums1212 <voll1212@gmail.com>
2024-07-12 11:27:17 +0900

    [Fix] #240 SqlStudyData 수정

--- app/src/main/java/kr/co/lion/modigm/model/SqlStudyData.kt
+++ app/src/main/java/kr/co/lion/modigm/model/SqlStudyData.kt
@@ -1,20 +1,22 @@
 package kr.co.lion.modigm.model
 
+import java.sql.ResultSet
+
 data class SqlStudyData(
-    var studyIdx: Int = -1,                     // 스터디 아이디
-    var studyTitle: String = "",                // 제목
-    var studyContent: String = "",              // 내용
-    var studyType: String = "",                 // 활동 타입 (스터디, 프로젝트, 공모전)
-    var studyPeriod: String = "",               // 진행 기간
-    var studyOnOffline: String = "",            // 진행 방식 (온라인, 오프라인, 온/오프 혼합)
-    var studyDetailPlace: String = "",          // 오프라인 시 진행 장소 상세 주소
-    var studyPlace: String = "",                // 오프라인 시 진행 장소
-    var studyApplyMethod: String = "",          // 신청 방식 (선착순, 신청제)
-    var studyCanApply: String = "",             // 모집 상태 (모집 중, 모집 완료)
-    var studyPic: String = "",                  // 썸네일 사진
-    var studyMaxMember: Int = 0,                // 최대 인원수
-    var studyState: Boolean = true,             // 삭제 여부 (존재함, 삭제됨)
-    var userIdx: Int = -1,                      // 사용자 번호
+    val studyIdx: Int = -1,                     // 스터디 아이디
+    val studyTitle: String = "",                // 제목
+    val studyContent: String = "",              // 내용
+    val studyType: String = "",                 // 활동 타입 (스터디, 프로젝트, 공모전)
+    val studyPeriod: String = "",               // 진행 기간
+    val studyOnOffline: String = "",            // 진행 방식 (온라인, 오프라인, 온/오프 혼합)
+    val studyDetailPlace: String = "",          // 오프라인 시 진행 장소 상세 주소
+    val studyPlace: String = "",                // 오프라인 시 진행 장소
+    val studyApplyMethod: String = "",          // 신청 방식 (선착순, 신청제)
+    val studyCanApply: String = "",             // 모집 상태 (모집 중, 모집 완료)
+    val studyPic: String = "",                  // 썸네일 사진
+    val studyMaxMember: Int = 0,                // 최대 인원수
+    val studyState: Boolean = true,             // 삭제 여부 (존재함, 삭제됨)
+    val userIdx: Int = -1,                      // 사용자 번호
 ){
     fun toMap(): Map<String, Any>{
         val map = mutableMapOf<String, Any>()
@@ -35,23 +37,41 @@ data class SqlStudyData(
     }
 
     companion object{
-        fun fromMap(map: Map<String, Any>): SqlStudyData{
-            val studyData = SqlStudyData()
-            studyData.studyTitle = map["studyTitle"] as String
-            studyData.studyContent = map["studyContent"] as String
-            studyData.studyType = map["studyType"] as String
-            studyData.studyPeriod = map["studyPeriod"] as String
-            studyData.studyOnOffline = map["studyOnOffline"] as String
-            studyData.studyDetailPlace = map["studyDetailPlace"] as String
-            studyData.studyPlace = map["studyPlace"] as String
-            studyData.studyApplyMethod = map["studyApplyMethod"] as String
-            studyData.studyCanApply = map["studyCanApply"] as String
-            studyData.studyPic = map["studyPic"] as String
-            studyData.studyMaxMember = map["studyMaxMember"] as Int
-            studyData.studyState = map["studyState"] as Boolean
-            studyData.userIdx = map["userIdx"] as Int
-            return studyData
+        fun fromMap(map: Map<String, Any>): SqlStudyData {
+            return SqlStudyData(
+                studyIdx = map["studyIdx"] as Int,
+                studyTitle = map["studyTitle"] as String,
+                studyContent = map["studyContent"] as String,
+                studyType = map["studyType"] as String,
+                studyPeriod = map["studyPeriod"] as String,
+                studyOnOffline = map["studyOnOffline"] as String,
+                studyDetailPlace = map["studyDetailPlace"] as String,
+                studyPlace = map["studyPlace"] as String,
+                studyApplyMethod = map["studyApplyMethod"] as String,
+                studyCanApply = map["studyCanApply"] as String,
+                studyPic = map["studyPic"] as String,
+                studyMaxMember = map["studyMaxMember"] as Int,
+                studyState = map["studyState"] as Boolean,
+                userIdx = map["userIdx"] as Int
+            )
+        }
+        fun getStudyData(resultSet: ResultSet): SqlStudyData{
+            return SqlStudyData(
+                resultSet.getInt("studyIdx"),
+                resultSet.getString("studyTitle"),
+                resultSet.getString("studyContent"),
+                resultSet.getString("studyType"),
+                resultSet.getString("studyPeriod"),
+                resultSet.getString("studyOnOffline"),
+                resultSet.getString("studyDetailPlace"),
+                resultSet.getString("studyPlace"),
+                resultSet.getString("studyApplyMethod"),
+                resultSet.getString("studyCanApply"),
+                resultSet.getString("studyPic"),
+                resultSet.getInt("studyMaxMember"),
+                resultSet.getBoolean("studyState"),
+                resultSet.getInt("userIdx"),
+            )
         }
     }
-
 }
