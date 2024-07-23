cdfe0702b9912104636c60622c737d073acec517
ums1212 <voll1212@gmail.com>
2024-07-23 20:29:03 +0900

    [Chore] #253 이름 변경2

--- app/src/main/java/kr/co/lion/modigm/repository/JoinUserRepository.kt
+++ app/src/main/java/kr/co/lion/modigm/repository/JoinUserRepository.kt
@@ -1,11 +1,11 @@
 package kr.co.lion.modigm.repository
 
-import kr.co.lion.modigm.db.join.JoinUserDataSource
+import kr.co.lion.modigm.db.join.RemoteJoinUserDataSource
 import kr.co.lion.modigm.model.SqlUserData
 
 class JoinUserRepository {
 
-    private val _joinUserDataSource = JoinUserDataSource()
+    private val _joinUserDataSource = RemoteJoinUserDataSource()
     // 회원가입
     suspend fun insetUserData(userInfoData: SqlUserData): Boolean = _joinUserDataSource.insetUserData(userInfoData)
 
