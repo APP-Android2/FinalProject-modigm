a7ec3675ce8113832a3d9b85402471322417bc37
jusungwon <tjddnjspower@naver.com>
2024-08-04 22:49:56 +0900

    [fix] 뷰바인딩 베이스 프래그먼트 상속으로 인한 바인딩 코드 수정.

--- app/src/main/java/kr/co/lion/modigm/ui/study/FilterSortFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/study/FilterSortFragment.kt
@@ -47,58 +47,58 @@ class FilterSortFragment : VBBaseFragment<FragmentFilterSortBinding>(FragmentFil
             }
 
             // 분류 칩그룹 초기화
-            setupChipGroup(binding.chipGroupBunryu, getChipsByCategory(Category.TYPE))
+            setupChipGroup(chipGroupBunryu, getChipsByCategory(Category.TYPE))
 
             // 기간 칩그룹 초기화 및 비가시성 설정
-            binding.layoutFilterGigan.visibility = View.GONE
-            setupChipGroup(binding.chipGroupGigan, getChipsByCategory(Category.PERIOD))
+            layoutFilterGigan.visibility = View.GONE
+            setupChipGroup(chipGroupGigan, getChipsByCategory(Category.PERIOD))
 
             // 분류 칩 클릭 이벤트
-            setChipClickListener(binding.chipGroupBunryu, binding.layoutFilterGigan)
+            setChipClickListener(chipGroupBunryu, layoutFilterGigan)
 
             // 장소 칩그룹 초기화 및 비가시성 설정
-            binding.layoutFilterJangso.visibility = View.GONE
-            setupChipGroup(binding.chipGroupJangso, getChipsByCategory(Category.ONOFFLINE))
+            layoutFilterJangso.visibility = View.GONE
+            setupChipGroup(chipGroupJangso, getChipsByCategory(Category.ONOFFLINE))
 
             // 기간 칩 클릭 이벤트
-            setChipClickListener(binding.chipGroupGigan, binding.layoutFilterJangso)
+            setChipClickListener(chipGroupGigan, layoutFilterJangso)
 
             // 인원수 칩그룹 초기화 및 비가시성 설정
-            binding.layoutFilterInwon.visibility = View.GONE
-            setupChipGroup(binding.chipGroupInwon, getChipsByCategory(Category.PEOPLE))
+            layoutFilterInwon.visibility = View.GONE
+            setupChipGroup(chipGroupInwon, getChipsByCategory(Category.PEOPLE))
 
             // 장소 칩 클릭 이벤트
-            setChipClickListener(binding.chipGroupJangso, binding.layoutFilterInwon)
+            setChipClickListener(chipGroupJangso, layoutFilterInwon)
 
             // 신청 방식 칩그룹 초기화 및 비가시성 설정
-            binding.layoutFilterSinchung.visibility = View.GONE
-            setupChipGroup(binding.chipGroupSinchung, getChipsByCategory(Category.APPLY_METHOD))
+            layoutFilterSinchung.visibility = View.GONE
+            setupChipGroup(chipGroupSinchung, getChipsByCategory(Category.APPLY_METHOD))
 
             // 인원수 칩 클릭 이벤트
-            setChipClickListener(binding.chipGroupInwon, binding.layoutFilterSinchung)
+            setChipClickListener(chipGroupInwon, layoutFilterSinchung)
 
             // 기술 스택 칩그룹 초기화 및 비가시성 설정
-            binding.layoutFilterGisul.visibility = View.GONE
-            setupChipGroup(binding.chipGroupGisul, getChipsByCategory(Category.TECH_STACK))
+            layoutFilterGisul.visibility = View.GONE
+            setupChipGroup(chipGroupGisul, getChipsByCategory(Category.TECH_STACK))
 
             // 신청 방식 칩 클릭 이벤트
-            setChipClickListener(binding.chipGroupSinchung, binding.layoutFilterGisul)
+            setChipClickListener(chipGroupSinchung, layoutFilterGisul)
 
             // 기술 스택 칩 클릭 이벤트
-            setTechStackChipClickListener(binding.chipGroupGisul, binding.layoutFilterPrograming)
+            setTechStackChipClickListener(chipGroupGisul, layoutFilterPrograming)
 
             // 프로그래밍 언어 칩그룹 초기화 및 비가시성 설정
-            binding.layoutFilterPrograming.visibility = View.GONE
-            setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.PROGRAMMING))
+            layoutFilterPrograming.visibility = View.GONE
+            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.PROGRAMMING))
 
-            setChipClickListener(binding.chipGroupPrograming, binding.layoutFilterPrograming)
+            setChipClickListener(chipGroupPrograming, layoutFilterPrograming)
 
             // 필터 적용 버튼 클릭 시
             buttonApplyFilter.setOnClickListener {
                 val filterData = mutableMapOf<String, String>()
 
                 // studyType 필터 데이터 설정
-                val selectedStudyTypeText = getSelectedChipText(binding.chipGroupBunryu)
+                val selectedStudyTypeText = getSelectedChipText(chipGroupBunryu)
                 Log.d("FilterSortFragment", "선택된 분류 칩 텍스트: $selectedStudyTypeText")
                 val studyTypeValue = selectedStudyTypeText?.let { displayName ->
                     FilterSort.fromDisplayName(displayName)?.value.toString()
@@ -114,7 +114,7 @@ class FilterSortFragment : VBBaseFragment<FragmentFilterSortBinding>(FragmentFil
                 Log.d("FilterSortFragment", "변환된 studyPeriod 값: $studyPeriodValue")
 
                 // studyOnOffline 필터 데이터 설정
-                val selectedStudyOnOfflineText = getSelectedChipText(binding.chipGroupJangso)
+                val selectedStudyOnOfflineText = getSelectedChipText(chipGroupJangso)
                 Log.d("FilterSortFragment", "선택된 장소 칩 텍스트: $selectedStudyOnOfflineText")
                 val studyOnOfflineValue = selectedStudyOnOfflineText?.let { displayName ->
                     FilterSort.fromDisplayName(displayName)?.value.toString()
@@ -122,7 +122,7 @@ class FilterSortFragment : VBBaseFragment<FragmentFilterSortBinding>(FragmentFil
                 Log.d("FilterSortFragment", "변환된 studyOnOffline 값: $studyOnOfflineValue")
 
                 // studyMaxMember 필터 데이터 설정
-                val selectedStudyMaxMemberText = getSelectedChipText(binding.chipGroupInwon)
+                val selectedStudyMaxMemberText = getSelectedChipText(chipGroupInwon)
                 Log.d("FilterSortFragment", "선택된 최대 인원수 칩 텍스트: $selectedStudyMaxMemberText")
                 val studyMaxMemberValue = selectedStudyMaxMemberText?.let { displayName ->
                     FilterSort.fromDisplayName(displayName)?.value.toString()
@@ -130,7 +130,7 @@ class FilterSortFragment : VBBaseFragment<FragmentFilterSortBinding>(FragmentFil
                 Log.d("FilterSortFragment", "변환된 studyMaxMember 값: $studyMaxMemberValue")
 
                 // studyApplyMethod 필터 데이터 설정
-                val selectedStudyApplyMethodText = getSelectedChipText(binding.chipGroupSinchung)
+                val selectedStudyApplyMethodText = getSelectedChipText(chipGroupSinchung)
                 Log.d("FilterSortFragment", "선택된 신청 방식 칩 텍스트: $selectedStudyApplyMethodText")
                 val studyApplyMethodValue = selectedStudyApplyMethodText?.let { displayName ->
                     FilterSort.fromDisplayName(displayName)?.value.toString()
@@ -138,7 +138,7 @@ class FilterSortFragment : VBBaseFragment<FragmentFilterSortBinding>(FragmentFil
                 Log.d("FilterSortFragment", "변환된 studyApplyMethod 값: $studyApplyMethodValue")
 
                 // studySkillList 필터 데이터 설정
-                val selectedStudySkillListText = getSelectedChipText(binding.chipGroupGisul)
+                val selectedStudySkillListText = getSelectedChipText(chipGroupGisul)
                 Log.d("FilterSortFragment", "선택된 기술 스택 칩 텍스트: $selectedStudySkillListText")
                 val studySkillListValue = selectedStudySkillListText?.let { displayName ->
                     FilterSort.fromDisplayName(displayName)?.value.toString()
@@ -146,7 +146,7 @@ class FilterSortFragment : VBBaseFragment<FragmentFilterSortBinding>(FragmentFil
                 Log.d("FilterSortFragment", "변환된 studySkillList 값: $studySkillListValue")
 
                 // programmingLanguage 필터 데이터 설정
-                val selectedProgrammingLanguageText = getSelectedChipText(binding.chipGroupPrograming)
+                val selectedProgrammingLanguageText = getSelectedChipText(chipGroupPrograming)
                 Log.d("FilterSortFragment", "선택된 프로그래밍 언어 칩 텍스트: $selectedProgrammingLanguageText")
                 val programmingLanguageValue = selectedProgrammingLanguageText?.let { displayName ->
                     FilterSort.fromDisplayName(displayName)?.value.toString()
@@ -171,7 +171,7 @@ class FilterSortFragment : VBBaseFragment<FragmentFilterSortBinding>(FragmentFil
 
     // 카테고리별 칩 리스트 생성
     private fun getChipsByCategory(category: Category): List<String> {
-        return FilterSort.values().filter { it.category == category }.map { it.displayName }
+        return FilterSort.entries.filter { it.category == category }.map { it.displayName }
     }
 
     // ChipGroup 초기화 함수
@@ -196,79 +196,82 @@ class FilterSortFragment : VBBaseFragment<FragmentFilterSortBinding>(FragmentFil
 
     // 기술 스택 칩 클릭 리스너 설정 함수
     private fun setTechStackChipClickListener(chipGroup: ChipGroup, targetLayout: View) {
-        chipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
-            if (checkedIds.isNotEmpty()) {
-                val checkedId = checkedIds[0]
-                val chip = group.findViewById<Chip>(checkedId)
-                updateVisibility(targetLayout, chip.isChecked)
-                updateChipStyles(chipGroup, checkedId)
-                Log.d("FilterSortFragment", "Chip 선택됨: ${chip.text}")
+        with(binding){
+            chipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+                if (checkedIds.isNotEmpty()) {
+                    val checkedId = checkedIds[0]
+                    val chip = group.findViewById<Chip>(checkedId)
+                    updateVisibility(targetLayout, chip.isChecked)
+                    updateChipStyles(chipGroup, checkedId)
+                    Log.d("FilterSortFragment", "Chip 선택됨: ${chip.text}")
 
-                binding.textViewFilterPrograming.text = "${chip.text}" // 여기서 TextView 업데이트
+                    textViewFilterPrograming.text = "${chip.text}" // 여기서 TextView 업데이트
 
-                when (chip.text) {
-                    "프로그래밍 언어" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.PROGRAMMING))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "프론트엔드" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.FRONT_END))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "백엔드" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.BACK_END))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "모바일개발" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.MOBILE))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "데이터과학" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.DATA_SCIENCE))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "데브옵스 및 시스템관리" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.DEVOPS))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "클라우드 및 인프라" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.CLOUD))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "게임개발" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.GAME_DEVELOPMENT))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "보안" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.SECURITY))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "인공지능" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.AI))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "UI/UX 디자인" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.UI_UX))
-                        scrollToView(binding.layoutFilterPrograming)
-                    }
-                    "빅데이터" -> {
-                        setupChipGroup(binding.chipGroupPrograming, getChipsByCategory(Category.BIG_DATA))
-                        scrollToView(binding.layoutFilterPrograming)
+                    when (chip.text) {
+                        "프로그래밍 언어" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.PROGRAMMING))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "프론트엔드" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.FRONT_END))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "백엔드" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.BACK_END))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "모바일개발" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.MOBILE))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "데이터과학" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.DATA_SCIENCE))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "데브옵스 및 시스템관리" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.DEVOPS))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "클라우드 및 인프라" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.CLOUD))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "게임개발" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.GAME_DEVELOPMENT))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "보안" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.SECURITY))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "인공지능" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.AI))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "UI/UX 디자인" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.UI_UX))
+                            scrollToView(layoutFilterPrograming)
+                        }
+                        "빅데이터" -> {
+                            setupChipGroup(chipGroupPrograming, getChipsByCategory(Category.BIG_DATA))
+                            scrollToView(layoutFilterPrograming)
+                        }
+
+                        // 다른 카테고리 추가...
+                        else -> updateVisibility(layoutFilterPrograming, false)
                     }
 
-                    // 다른 카테고리 추가...
-                    else -> updateVisibility(binding.layoutFilterPrograming, false)
+                    updateVisibility(layoutFilterPrograming, chip.text != "전체" && chip.isChecked)
+                } else {
+                    updateVisibility(targetLayout, false)
+                    updateVisibility(layoutFilterPrograming, false)
+                    updateChipStyles(chipGroup, -1) // Reset all chip styles
+                    Log.d("FilterSortFragment", "Chip 선택 해제됨")
                 }
-
-                updateVisibility(binding.layoutFilterPrograming, chip.text != "전체" && chip.isChecked)
-            } else {
-                updateVisibility(targetLayout, false)
-                updateVisibility(binding.layoutFilterPrograming, false)
-                updateChipStyles(chipGroup, -1) // Reset all chip styles
-                Log.d("FilterSortFragment", "Chip 선택 해제됨")
+                updateApplyButtonState()
             }
-            updateApplyButtonState()
         }
+
     }
 
     private fun setChipClickListener(chipGroup: ChipGroup, targetLayout: View) {
@@ -322,31 +325,37 @@ class FilterSortFragment : VBBaseFragment<FragmentFilterSortBinding>(FragmentFil
 
     // 스크롤 이동 함수
     private fun scrollToView(targetView: View) {
-        binding.root.post {
-            binding.scrollViewFilterSort.smoothScrollTo(0, targetView.top)
+        with(binding){
+            root.post {
+                scrollViewFilterSort.smoothScrollTo(0, targetView.top)
+            }
         }
+
     }
 
     // 추가된 부분: 적용 버튼 상태 업데이트 함수
     private fun updateApplyButtonState() {
-        val hasActiveChip = listOf(
-            binding.chipGroupBunryu,
-            binding.chipGroupGigan,
-            binding.chipGroupJangso,
-            binding.chipGroupInwon,
-            binding.chipGroupSinchung,
-            binding.chipGroupGisul,
-            binding.chipGroupPrograming
-        ).any { it.checkedChipId != View.NO_ID }
+        with(binding){
+            val hasActiveChip = listOf(
+                chipGroupBunryu,
+                chipGroupGigan,
+                chipGroupJangso,
+                chipGroupInwon,
+                chipGroupSinchung,
+                chipGroupGisul,
+                chipGroupPrograming
+            ).any { it.checkedChipId != View.NO_ID }
 
-        binding.buttonApplyFilter.apply {
-            isEnabled = hasActiveChip
-            backgroundTintList = ColorStateList.valueOf(
-                ContextCompat.getColor(
-                    context,
-                    if (hasActiveChip) R.color.pointColor else R.color.buttonGray
+            buttonApplyFilter.apply {
+                isEnabled = hasActiveChip
+                backgroundTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        context,
+                        if (hasActiveChip) R.color.pointColor else R.color.buttonGray
+                    )
                 )
-            )
+            }
         }
+
     }
 }
