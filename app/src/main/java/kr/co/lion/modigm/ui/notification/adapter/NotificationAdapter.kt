d224254c014f905fdfe2282b11b158c233dd6b10
Lee SeungHyun <leesh0545@gmail.com>
2024-09-09 00:42:53 +0900

    [feat #308] detailFragment이동 추가

--- app/src/main/java/kr/co/lion/modigm/ui/notification/adapter/NotificationAdapter.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/notification/adapter/NotificationAdapter.kt
@@ -1,12 +1,16 @@
 package kr.co.lion.modigm.ui.notification.adapter
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kr.co.lion.modigm.R
 import kr.co.lion.modigm.databinding.RowNotificationBinding
 import kr.co.lion.modigm.model.NotificationData
+import kr.co.lion.modigm.ui.detail.DetailFragment
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -49,6 +53,24 @@ class NotificationAdapter(
             // X 아이콘 클릭 리스너 설정
             binding.imageViewDeleteNotification.setOnClickListener {
                 onDeleteClick(notification) // X 아이콘 클릭 시 콜백 호출
+            }
+
+            // 항목 클릭 리스너 설정
+            binding.root.setOnClickListener {
+                val studyIdx = notification.studyIdx
+
+                // Fragment로 이동하는 로직 추가
+                val activity = it.context as? AppCompatActivity // Context를 AppCompatActivity로 캐스팅
+                val detailFragment = DetailFragment().apply {
+                    arguments = Bundle().apply {
+                        putInt("studyIdx", studyIdx)
+                    }
+                }
+
+                activity?.supportFragmentManager?.commit {
+                    replace(R.id.containerMain, detailFragment)
+                    addToBackStack("DETAIL") // Fragment 이름을 추가하여 백스택에 저장
+                }
             }
         }
     }
