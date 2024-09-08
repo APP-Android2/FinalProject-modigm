2e8dfec5ff9639bcfec7e40d447d76d358574225
Lee SeungHyun <leesh0545@gmail.com>
2024-09-08 22:50:59 +0900

    [feat #308] notification model

---
+++ app/src/main/java/kr/co/lion/modigm/model/NotificationData.kt
@@ -0,0 +1,26 @@
+package kr.co.lion.modigm.model
+
+import java.sql.ResultSet
+import java.util.Date
+
+data class NotificationData(
+    val notificationIdx: Int,
+    val userIdx: Int,
+    val notificationTitle: String,
+    val notificationContent: String,
+    val coverPhotoUrl: String?,
+    val notificationTime: Date
+) {
+    companion object {
+        fun getNotificationData(resultSet: ResultSet): NotificationData {
+            return NotificationData(
+                notificationIdx = resultSet.getInt("notificationIdx"),
+                userIdx = resultSet.getInt("userIdx"),
+                notificationTitle = resultSet.getString("notificationTitle"),
+                notificationContent = resultSet.getString("notificationContent"),
+                coverPhotoUrl = resultSet.getString("cover_photo_url"),
+                notificationTime = resultSet.getTimestamp("notificationTime")
+            )
+        }
+    }
+}
