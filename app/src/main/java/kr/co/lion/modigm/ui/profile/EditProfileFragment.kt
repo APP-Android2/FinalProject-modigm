1e0711f1fd9ccf37ef50fe188da352519fa6a66b
zunione <xt.wish1@gmail.com>
2024-09-14 00:11:05 +0900

    feature #338: 드롭다운에서 사진 변경 정상 동작

--- app/src/main/java/kr/co/lion/modigm/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/profile/EditProfileFragment.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,6 +20,7 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.FileProvider
 import androidx.core.view.children
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
@@ -42,12 +45,20 @@ import kr.co.lion.modigm.util.FragmentName
 import kr.co.lion.modigm.util.JoinType
 import kr.co.lion.modigm.util.ModigmApplication.Companion.prefs
 import kr.co.lion.modigm.util.Picture
+import java.io.File
 
-class EditProfileFragment(private val profileFragment: ProfileFragment): DBBaseFragment<FragmentEditProfileBinding>(R.layout.fragment_edit_profile) {
+class EditProfileFragment: DBBaseFragment<FragmentEditProfileBinding>(R.layout.fragment_edit_profile) {
     private val editProfileViewModel: EditProfileViewModel by activityViewModels()
 
     // 어댑터 선언
     private lateinit var linkAddAdapter: LinkAddAdapter
+
+    // 카메라 실행을 위한 런처
+    private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
+    // 앨범 실행을 위한 런처
+    private lateinit var albumLauncher: ActivityResultLauncher<Intent>
+    // 촬영된 사진이 저장된 경로 정보를 가지고 있는 Uri 객체
+    private lateinit var photoUri: Uri
 
     // 확인할 권한 목록
     private val permissionList = arrayOf(
@@ -60,13 +71,84 @@ class EditProfileFragment(private val profileFragment: ProfileFragment): DBBaseF
         binding.editProfileViewModel = editProfileViewModel
         binding.lifecycleOwner = this
 
+
+
         return binding.root
+    }
+
+    private fun setupCameraLauncher() {
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            // 사진을 사용하겠다고 한 다음에 돌아왔을 경우
+            if(it.resultCode == AppCompatActivity.RESULT_OK) {
+                // 사진 객체를 생성한다.
+                val bitmap = BitmapFactory.decodeFile(photoUri.path)
+                binding.imageProfilePic.setImageBitmap(bitmap)
+
+                // 회전 각도값을 구한다.
+                val degree = Picture.getDegree(requireContext(), photoUri)
+                // 회전된 이미지를 구한다.
+                val bitmap2 = Picture.rotateBitmap(bitmap, degree.toFloat())
+                // 크기를 조정한 이미지를 구한다.
+                val bitmap3 = Picture.resizeBitmap(bitmap2, 1024)
+
+                binding.imageProfilePic.setImageBitmap(bitmap3)
+                editProfileViewModel.editProfilePicUri.value = photoUri
+                editProfileViewModel.picChanged = true
+            }
+        }
+    }
+
+    private fun setupAlbumLauncher() {
+        albumLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            // 사진 선택을 완료한 후 돌아왔다면
+            if(it.resultCode == AppCompatActivity.RESULT_OK) {
+                // 선택한 이미지의 경로 데이터를 관리하는 Uri 객체를 추출한다.
+                val imageUri = it.data?.data
+                if(imageUri != null){
+                    // 안드로이드 Q(10) 이상이라면
+                    val bitmap = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                        // 이미지를 생성할 수 있는 객체를 생성한다.
+                        val source = ImageDecoder.createSource(requireContext().contentResolver, imageUri)
+                        // Bitmap을 생성한다.
+                        ImageDecoder.decodeBitmap(source)
+                    } else {
+                        // 컨텐츠 프로바이더를 통해 이미지 데이터에 접근한다.
+                        val cursor = requireContext().contentResolver.query(imageUri, null, null, null, null)
+                        if(cursor != null){
+                            cursor.moveToNext()
+
+                            // 이미지의 경로를 가져온다.
+                            val idx = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+                            val source = cursor.getString(idx)
+
+                            // 이미지를 생성한다
+                            BitmapFactory.decodeFile(source)
+                        }  else {
+                            null
+                        }
+                    }
+
+                    // 회전 각도값을 가져온다.
+                    val degree = Picture.getDegree(requireContext(), imageUri)
+                    // 회전 이미지를 가져온다
+                    val bitmap2 = Picture.rotateBitmap(bitmap!!, degree.toFloat())
+                    // 크기를 줄인 이미지를 가져온다.
+                    val bitmap3 = Picture.resizeBitmap(bitmap2, 1024)
+
+                    binding.imageProfilePic.setImageBitmap(bitmap3)
+                    editProfileViewModel.editProfilePicUri.value = imageUri
+                    editProfileViewModel.picChanged = true
+                }
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requestPermissions(permissionList, 0)
         initView()
+        setupCameraLauncher()
+        setupAlbumLauncher()
     }
 
     private fun initView() {
@@ -130,13 +212,45 @@ class EditProfileFragment(private val profileFragment: ProfileFragment): DBBaseF
 
         item1.setOnClickListener {
             // 아이템 1이 클릭되었을 때의 처리
+            startCameraLauncher()
             popupWindow.dismiss()
         }
 
         item2.setOnClickListener {
             // 아이템 2가 클릭되었을 때의 처리
+            startAlbumLauncher()
             popupWindow.dismiss()
         }
+    }
+
+    // 카메라 런처를 실행하는 메서드
+    // step1) res/xml 폴더에 xml 파일을 만들어주고 이 파일에 사진이 저장될 외부저장소까지의 경로를 기록해준다.
+    // step2) AndroidMenifest.xml 에 1에서 만든 파일의 경로를 지정해 준다.
+    fun startCameraLauncher() {
+        // 촬영한 사진이 저장될 경로
+        val file = File(requireContext().getExternalFilesDir(null), "tempImage.jpg")
+        photoUri = FileProvider.getUriForFile(requireContext(), "kr.co.lion.modigm.file_provider", file)
+
+        // 카메라 인텐트에서 MediaStore.EXTRA_OUTPUT을 통해 이미지가 저장된 파일 경로를 지정하면
+        // 실제 이미지 파일은 지정한 경로에 저장되고, 인텐트의 반환 데이터로 이미지가 직접 포함되지 않는다
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+
+        cameraLauncher.launch(cameraIntent)
+    }
+
+    // 앨범 런처를 실행하는 메서드
+    fun startAlbumLauncher(){
+        // 앨범에서 사진을 선택할 수 있도록 셋팅된 인텐트를 생성한다.
+        val albumIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        // 실행할 액티비티의 타입을 설정(이미지를 선택할 수 있는 것이 뜨게 한다)
+        albumIntent.setType("image/*")
+        // 선택할 수 있는 파일들의 MimeType을 설정한다.
+        // 여기서 선택한 종류의 파일만 선택이 가능하다. 모든 이미지로 설정한다.
+        val mimeType = arrayOf("image/*")
+        albumIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeType)
+        // 액티비티를 실행한다.
+        albumLauncher.launch(albumIntent)
     }
 
     private fun setupButtonChangePhone() {
@@ -202,8 +316,8 @@ class EditProfileFragment(private val profileFragment: ProfileFragment): DBBaseF
     private fun setupButtonDone() {
         binding.buttonEditProfileDone.setOnClickListener {
             // 데이터베이스 업데이트
-            editProfileViewModel.updateUserData(profileFragment, editProfileViewModel.picChanged, requireContext())
-            editProfileViewModel.updateUserLinkData(profileFragment)
+            editProfileViewModel.updateUserData(editProfileViewModel.picChanged, requireContext())
+            editProfileViewModel.updateUserLinkData()
 
             // 스낵바 띄우기
             val snackbar = Snackbar.make(binding.root, "정보가 업데이트되었습니다.", Snackbar.LENGTH_LONG)
