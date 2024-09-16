fe0371641327c084bc0fcadcd6c36b88e17fdc68
ums1212 <voll1212@gmail.com>
2024-09-16 22:23:39 +0900

    [Feat] #345 이메일 인증 프래그먼트 추가

---
+++ app/src/main/java/kr/co/lion/modigm/ui/join/JoinEmailVerificationFragment.kt
@@ -0,0 +1,6 @@
+package kr.co.lion.modigm.ui.join
+
+import kr.co.lion.modigm.databinding.FragmentJoinEmailVerificationBinding
+import kr.co.lion.modigm.ui.VBBaseFragment
+
+class JoinEmailVerificationFragment : VBBaseFragment<FragmentJoinEmailVerificationBinding>(FragmentJoinEmailVerificationBinding::inflate)
