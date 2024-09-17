4c68d3b17605a58b7b57c61e1b7be04d7a15fabf
zunione <xt.wish1@gmail.com>
2024-09-17 22:22:42 +0900

    feature #350: 로그아웃 애드 다이얼로그에 loadAd

--- app/src/main/java/kr/co/lion/modigm/ui/profile/popup/LogoutAdDialog.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/profile/popup/LogoutAdDialog.kt
@@ -8,14 +8,42 @@ import android.os.Process
 import android.view.View
 import android.widget.TextView
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kr.co.lion.modigm.databinding.CustomDialogBinding
+import kr.co.lion.modigm.databinding.CustomDialogLogoutAdBinding
 
 
 class LogoutAdDialog(context: Context) : Dialog(context) {
-    val binding = CustomDialogBinding.inflate(layoutInflater)
+    val binding = CustomDialogLogoutAdBinding.inflate(layoutInflater)
 
     override fun onCreate(savedInstanceState: Bundle) {
         super.onCreate(savedInstanceState)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            MobileAds.initialize(context)
+        }
+
+        setupAdMob()
+        setupButtonLogout()
+        setupButtonQuit()
+    }
+
+    fun setupAdMob() {
+        // Start loading the ad in the background.
+        val adRequest = AdRequest.Builder().build()
+        binding.adViewLogoutDialog.loadAd(adRequest)
+
+    }
+
+    fun setupButtonLogout() {
+        val
+    }
+
+    fun setupButtonQuit() {
+        val
     }
 }
