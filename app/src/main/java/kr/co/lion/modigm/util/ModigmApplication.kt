56be47f80360c2977e50bdb5aaf2b2d918f9cd86
jusungwon <tjddnjspower@naver.com>
2024-09-21 19:08:39 +0900

    [Fix] 로그아웃 다이얼로그 디자인 통일 + 애드 프리로드 설정

--- app/src/main/java/kr/co/lion/modigm/util/ModigmApplication.kt
+++ app/src/main/java/kr/co/lion/modigm/util/ModigmApplication.kt
@@ -2,6 +2,7 @@ package kr.co.lion.modigm.util
 
 import android.app.Application
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
@@ -34,9 +35,10 @@ class ModigmApplication : Application() {
     // 배너 광고를 미리 로드하는 함수
     private fun preloadAdBanner() {
         // AdView 생성 및 설정
+        val adSize = AdSize.getCurrentOrientationInlineAdaptiveBannerAdSize(this, 320)
         preloadedAdView = AdView(this).apply {
-            adUnitId = "ca-app-pub-7493119982793962/1062136620" // 실제 광고 단위 ID로 변경
-            setAdSize(com.google.android.gms.ads.AdSize.BANNER) // adSize는 여기서 설정
+            adUnitId = "ca-app-pub-3940256099942544/9214589741" // 실제 광고 단위 ID로 변경
+            setAdSize(adSize) // adSize는 여기서 설정
         }
 
         // 광고 요청을 생성하고 로드
