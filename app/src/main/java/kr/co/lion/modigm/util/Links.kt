a6b318c33b84f88a67af7884fdf68bcdc37903e3
ums1212 <voll1212@gmail.com>
2024-10-05 15:10:59 +0900

    [Feat] #396 회원, 스터디 구분하여 신고하기 구글폼 연결

--- app/src/main/java/kr/co/lion/modigm/util/Links.kt
+++ app/src/main/java/kr/co/lion/modigm/util/Links.kt
@@ -2,6 +2,23 @@ package kr.co.lion.modigm.util
 
 enum class Links(val url:String) {
     NOTICE("https://smooth-orangutan-e69.notion.site/0ff98834d23f43269dfab13e15ee88fb?pvs=4"),
-    SERVICE("https://forms.gle/J1qcXTYDzcgB69wQ6"),
+    SERVICE("https://docs.google.com/forms/d/e/1FAIpQLSdqxQHUyBrWro9b_MUJUerrCA4tlLuHQEwQeJhaiJm3q1rvFA/viewform?usp=sf_link"),
+    USER_SERVICE("https://docs.google.com/forms/d/e/1FAIpQLSdpbl03LqV9APHx04ehsJRJUzEXSrHWCTb2HyAD4WbCGxzbKw/viewform?usp=sf_link"),
+    STUDY_SERVICE("https://docs.google.com/forms/d/e/1FAIpQLSdr9AuE2vrGMpQTxmdo7jUYZCRZ5ZjggR5bswbDjqSSgYuhKg/viewform?usp=sf_link"),
     TERMS("https://smooth-orangutan-e69.notion.site/5c47ca9800114574898502f04b17595f?pvs=4"),
 }
+
+// 고객 센터
+// https://docs.google.com/forms/d/e/1FAIpQLSdqxQHUyBrWro9b_MUJUerrCA4tlLuHQEwQeJhaiJm3q1rvFA/viewform?usp=sf_link
+// 성함 : entry.2005620554
+// 이메일 : entry.1045781291
+
+// 스터디 신고
+// https://docs.google.com/forms/d/e/1FAIpQLSdr9AuE2vrGMpQTxmdo7jUYZCRZ5ZjggR5bswbDjqSSgYuhKg/viewform?usp=sf_link
+// 신고 번호1(스터디 IDX) : entry.1666022374
+// 신고 번호2(신고자 IDX) : entry.218705015
+
+// 회원 신고
+// https://docs.google.com/forms/d/e/1FAIpQLSdpbl03LqV9APHx04ehsJRJUzEXSrHWCTb2HyAD4WbCGxzbKw/viewform?usp=sf_link
+// 신고 번호1(신고 대상 IDX) : entry.1881471803
+// 신고 번호2(신고자 IDX) : entry.759987217
