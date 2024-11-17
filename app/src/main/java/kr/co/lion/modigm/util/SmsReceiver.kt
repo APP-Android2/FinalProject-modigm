1dde0f09597d978f8e8dd4fd4fee178f25fac574
ums1212 <voll1212@gmail.com>
2024-11-17 13:51:08 +0900

    [Fix] #404 코드 수정

--- app/src/main/java/kr/co/lion/modigm/util/SmsReceiver.kt
+++ app/src/main/java/kr/co/lion/modigm/util/SmsReceiver.kt
@@ -5,8 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
-import android.provider.Telephony
-import android.util.Log
 import com.google.android.gms.auth.api.phone.SmsRetriever
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.api.Status
@@ -14,7 +12,7 @@ import com.google.android.gms.common.api.Status
 class SmsReceiver: BroadcastReceiver() {
 
     companion object {
-        private const val PATTERN = "^modigm-4afde\\.firebaseapp\\.com"
+        private const val PATTERN = "\\d{6}"
     }
 
     interface SmsReceiverListener {
@@ -24,24 +22,21 @@ class SmsReceiver: BroadcastReceiver() {
     private var smsListener: SmsReceiverListener? = null
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        if(intent?.action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
+
+        if(intent?.action == SmsRetriever.SMS_RETRIEVED_ACTION) {
             intent.extras?.let { bundle ->
                 val status = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
                     bundle.getParcelable(SmsRetriever.EXTRA_STATUS, Status::class.java) as Status
                 }else{
                     bundle.get(SmsRetriever.EXTRA_STATUS) as Status
                 }
-
                 when(status.statusCode) {
                     CommonStatusCodes.SUCCESS -> {
                         // 받아온 문자 내용
                         val sms = bundle.getString(SmsRetriever.EXTRA_SMS_MESSAGE, "")
-                        if(smsListener != null && sms.isNotEmpty()){
-                            val check = PATTERN.toRegex().find(sms)?.destructured?.component1()
-                            if(!check.isNullOrEmpty()){
-                                Log.d("test1234", "$check")
-                                smsListener!!.onMessageReceived(check)
-                            }
+                        val authCode = getAuthCode(sms)
+                        if(smsListener != null && authCode != null){
+                            smsListener!!.onMessageReceived(authCode)
                         }
                     }
                 }
@@ -57,4 +52,6 @@ class SmsReceiver: BroadcastReceiver() {
     fun doFilter() = IntentFilter().apply {
         addAction(SmsRetriever.SMS_RETRIEVED_ACTION)
     }
+
+    private fun getAuthCode(message: String): String? = PATTERN.toRegex().find(message)?.destructured?.component1()
 }
