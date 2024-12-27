6746567a27f19902560cae50257b6dbd9773f683
jusungwon <tjddnjspower@naver.com>
2024-12-28 03:51:26 +0900

    [Refactor] EmailLoginLoading 컴포넌트 구현

---
+++ app/src/main/java/kr/co/lion/modigm/ui/login/email/component/EmailLoginLoading.kt
@@ -0,0 +1,29 @@
+package kr.co.lion.modigm.ui.login.email.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+
+@Composable
+fun EmailLoginLoading(
+    modifier: Modifier = Modifier,
+    isLoading: Boolean
+) {
+    if (isLoading) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.5f))
+                .pointerInput(Unit) { },
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(color = Color.White)
+        }
+    }
+}
