b52291ee97d4d726d53375f9205a8552fb28f2a2
jusungwon <tjddnjspower@naver.com>
2024-12-29 03:01:14 +0900

    [Refactor] 이메일 로그인 버튼 컴포넌트 생성

---
+++ app/src/main/java/kr/co/lion/modigm/ui/login/email/component/EmailLoginButton.kt
@@ -0,0 +1,33 @@
+package kr.co.lion.modigm.ui.login.email.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import kr.co.lion.modigm.R
+import kr.co.lion.modigm.ui.login.email.dpToSp
+
+@Composable
+fun EmailLoginButton(
+    onEmailLoginButtonClick: () -> Unit
+) {
+    Button(
+        onClick = { onEmailLoginButtonClick() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp),
+        colors = buttonColors(Color(ContextCompat.getColor(LocalContext.current, R.color.pointColor)))
+    ) {
+        Text(
+            text = "로그인",
+            fontSize = dpToSp(16.dp)
+        )
+    }
+}
