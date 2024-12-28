12ec8afc542b7e754ce9741550c7a1d4c500f822
jusungwon <tjddnjspower@naver.com>
2024-12-28 23:08:18 +0900

    [Refactor] EmailAutoLoginCheckBox 구현 1. 체크박스 구현 2. 체크박스 옆 텍스트 구현 3. isChecked 상태 전달 구현

---
+++ app/src/main/java/kr/co/lion/modigm/ui/login/email/component/EmailAutoLoginCheckBox.kt
@@ -0,0 +1,35 @@
+package kr.co.lion.modigm.ui.login.email.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kr.co.lion.modigm.ui.login.social.dpToSp
+
+@Composable
+fun EmailAutoLoginCheckBox(
+    isChecked: MutableState<Boolean>
+) {
+    Checkbox(
+        checked = isChecked.value,
+        onCheckedChange = { isChecked.value = it },
+        modifier = Modifier
+            .size(20.dp)
+            .padding(top = 0.dp, bottom = 0.dp, start = 0.dp, end = 0.dp)
+
+    )
+    Spacer(modifier = Modifier
+        .size(8.dp)
+        .clickable { isChecked.value = !isChecked.value })
+    Text(
+        text = "자동 로그인",
+        fontSize = dpToSp(16.dp),
+        modifier = Modifier.clickable { isChecked.value = !isChecked.value }
+    )
+}
