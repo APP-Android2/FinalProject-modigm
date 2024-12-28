6e4138d7ff000d6d708a23a8bb5f84762219719b
jusungwon <tjddnjspower@naver.com>
2024-12-28 23:09:59 +0900

    [Refactor] 이메일찾기와 비밀번호 찾기 버튼 구현 1. FindEmailTextButton 컴포넌트 구현 2. FindPasswordTextButton 컴포넌트 구현 3. 화면전환 상태 구현

---
+++ app/src/main/java/kr/co/lion/modigm/ui/login/email/component/FindEmailTextButton.kt
@@ -0,0 +1,26 @@
+package kr.co.lion.modigm.ui.login.email.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun FindEmailTextButton(
+    onNavigateToFindEmailFragment: () -> Unit
+) {
+    TextButton(
+        onClick = { onNavigateToFindEmailFragment() },
+        modifier = Modifier.padding(end = 4.dp)
+    ) {
+        Text(
+            text = "이메일 찾기",
+            color = Color.Black,
+            fontSize = 16.sp
+        )
+    }
+}
---
+++ app/src/main/java/kr/co/lion/modigm/ui/login/email/component/FindPasswordTextButton.kt
@@ -0,0 +1,25 @@
+package kr.co.lion.modigm.ui.login.email.component
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun FindPasswordTextButton(
+    onNavigateToFindPasswordFragment: () -> Unit
+) {
+    TextButton(
+        onClick = { onNavigateToFindPasswordFragment() },
+        contentPadding = PaddingValues(start = 16.dp, end = 0.dp)
+    ) {
+        Text(
+            text = "비밀번호 찾기",
+            color = Color.Black,
+            fontSize = 16.sp
+        )
+    }
+}
