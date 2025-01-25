fdfcf4fa32cf67201cf17e82602dc1dc8c5ce97c
ums1212 <voll1212@gmail.com>
2025-01-25 14:05:50 +0900

    [Refactor] #428 뷰바인딩 대신 컴포즈뷰 반환으로 변경

--- app/src/main/java/kr/co/lion/modigm/ui/join/JoinStep1EmailAndPwFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/join/JoinStep1EmailAndPwFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import kr.co.lion.modigm.databinding.FragmentJoinStep1EmailAndPwBinding
-import kr.co.lion.modigm.ui.VBBaseFragment
-import kr.co.lion.modigm.ui.join.component.JoinStep1EmailAndPwScreen
 import kr.co.lion.modigm.ui.join.vm.JoinStep1EmailAndPwViewModel
 
-class JoinStep1EmailAndPwFragment : VBBaseFragment<FragmentJoinStep1EmailAndPwBinding>(FragmentJoinStep1EmailAndPwBinding::inflate) {
+class JoinStep1EmailAndPwFragment : Fragment() {
 
     private val joinStep1EmailAndPwViewModel: JoinStep1EmailAndPwViewModel by activityViewModels()
 
@@ -22,15 +21,12 @@ class JoinStep1EmailAndPwFragment : VBBaseFragment<FragmentJoinStep1EmailAndPwBi
         // Inflate the layout for this fragment
         super.onCreateView(inflater, container, savedInstanceState)
 
-        binding.composeViewJoinStep1EmailAndPw.apply {
+        return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 JoinStep1EmailAndPwScreen(joinStep1EmailAndPwViewModel)
             }
         }
-        return binding.root
     }
-
-
 
 }
