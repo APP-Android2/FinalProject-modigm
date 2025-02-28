95a8fd733b8ef6216ca043b0a5556cad91d68918
ums1212 <voll1212@gmail.com>
2025-02-28 21:00:45 +0900

    [Refactor] #442 JoinEmailVerificationFragment의 xml뷰를 compose로 변경

--- app/src/main/java/kr/co/lion/modigm/ui/join/JoinEmailVerificationFragment.kt
+++ app/src/main/java/kr/co/lion/modigm/ui/join/JoinEmailVerificationFragment.kt
@@ -1,6 +1,27 @@
 package kr.co.lion.modigm.ui.join
 
-import kr.co.lion.modigm.databinding.FragmentJoinEmailVerificationBinding
-import kr.co.lion.modigm.ui.VBBaseFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
 
-class JoinEmailVerificationFragment : VBBaseFragment<FragmentJoinEmailVerificationBinding>(FragmentJoinEmailVerificationBinding::inflate)
+class JoinEmailVerificationFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                JoinEmailVerificationScreen()
+            }
+        }
+    }
+}
